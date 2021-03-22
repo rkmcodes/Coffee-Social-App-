dfe3890953fee58a6220017fd1fd9355da9da2a3
Kelly McCullough <72524184+rkmcodes@users.noreply.github.com>
2021-03-22 17:24:47 -0500

    Add files via upload
    this is a hello spring demo project with 2 controllers, on localhost 8080 it will display "hello world" & with 8080/goodbye it displays "goodbye"

---
+++ hello-spring/src/main/java/com/launchcode/hellospring/controllers/GoodbyeController.java
@@ -0,0 +1,16 @@
+package com.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GoodbyeController {
+
+    @RequestMapping(value="goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye";
+    }
+
+}
